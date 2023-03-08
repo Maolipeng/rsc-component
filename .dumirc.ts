6e3148fbd484d05fbbca1045dc29fb78dc9e740b
Maolipeng <m384861046@gmail.com>
2023-03-08 16:47:12 +0800

    docs: 增加源码链接

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,12 +4,15 @@ export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'RscComponent',
+    socialLinks: {
+      github: 'https://github.com/Maolipeng/rsc-component',
+    },
   },
   styles: [
     `
     .dumi-default-header-left {
       width: 230px !important;
     }
-    `
-  ]
+    `,
+  ],
 });
